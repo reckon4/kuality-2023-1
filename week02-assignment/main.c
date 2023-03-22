384e23f3a48d6d991bc78507f8ccbe65f1421318
SungKwon Lee <sooips147@gmail.com>
2023-03-22 20:50:47 +0900

    week02-assignment

---
+++ week02-assignment/main.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int id = 0;
+
+	printf("HI");
+	scanf("%d", &id);
+	
+	printf("Hi %d !", id);
+
+	return 0;
+
+
+
+
+
+
+	return 0;
+}
